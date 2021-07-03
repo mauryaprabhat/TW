d53e13da46d83a3c65d716ee75a140d21d17649a
Maurya, Prabhat <mauryaprabhat2@gmail.com>
2021-07-03 08:22:20 +0530

    First commmit

---
+++ JOIEnergy/Controllers/CostController.cs
@@ -0,0 +1,28 @@
+﻿using JOIEnergy.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JOIEnergy.Controllers
+{
+    [Route("Cost")]
+    public class CostController:Controller
+    {
+        private ICostService _costService;
+        public CostController(ICostService costService)
+        {
+            this._costService = costService;
+        }
+
+        [HttpGet("Cost/{smartMeterId}")]
+
+        public ObjectResult Cost(string smartMeterId)
+        {
+           return _costService.Cost(smartMeterId);
+           // return null;
+        }
+
+    }
+}
---
+++ JOIEnergy/Services/CostService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JOIEnergy.Domain;
+using JOIEnergy.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JOIEnergy.Services
+{
+    public class CostService : ICostService
+    {
+        public Dictionary<string, List<ElectricityReading>> MeterAssociatedReadings { get; set; }
+
+        AccountService accountService;
+        public CostService(Dictionary<string, List<ElectricityReading>> meterAssociatedReadings)
+        {
+
+        }
+        public decimal Cost(string smartMeterId)
+        {
+            if (!String.IsNullOrWhiteSpace(smartMeterId))
+            {
+                Supplier result = accountService.GetPricePlanIdForSmartMeterId(smartMeterId);
+                PricePlan pricePlan;
+                //check the priceplan
+                //if (smartMeterId.Conta)
+
+                 //calcualte
+                 
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
